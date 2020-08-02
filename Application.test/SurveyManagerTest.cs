eb42da1bac745d00e8313795e9083a2ba638d5c8
Reinaldo Ospino <reinaldo.aospino@gmail.com>
2020-08-02 13:20:55 -0300

    se agregan los tests unitarios al SurveyManager

---
+++ Application.test/SurveyManagerTest.cs
@@ -0,0 +1,135 @@
+﻿using Moq;
+using Xunit;
+using System.Linq;
+using Domain.Entities;
+using Application.Managers;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Domain.Interfaces.Application;
+using Domain.Interfaces.Infraestructure;
+
+namespace Application.test
+{
+    public class SurveyManagerTest
+    {
+        private readonly Mock<ISurveyRepository> _mockRepository;
+
+        private ISurveyManager _surveyManager;
+
+        public SurveyManagerTest()
+        {
+            _mockRepository = new Mock<ISurveyRepository>();
+            _surveyManager = new SurveyManager(_mockRepository.Object);
+        }
+
+        [Fact]
+        public async Task WhenGetSurvey_ThenGetSuccessful()
+        {
+            //?Given
+            var surveyList = new List<Survey>
+            {
+                new Survey
+                {
+                    Id = "1234",
+                    Question = "This is a question",
+                    Options = new List<SurveyOption>
+                    {
+                        new SurveyOption
+                        {
+                             Option = "This is an option"
+                        }
+                    }
+                }
+            };
+
+            _mockRepository.Setup(x => x.Get())
+                .ReturnsAsync(surveyList)
+                .Verifiable();
+
+            //?When 
+            var result = await _surveyManager.Get();
+
+            //?Then
+            var expected = surveyList.FirstOrDefault();
+            var actual = result.ToList().FirstOrDefault();
+
+            Assert.Equal(expected.Id, actual.Id);
+            Assert.Equal(expected.Question, actual.Question);
+            Assert.Equal(expected.Options.FirstOrDefault().Option, actual.Options.FirstOrDefault().Option);
+
+            _mockRepository.VerifyAll();
+        }
+
+        [Fact]
+        public async Task GivenSurvey_WhenCreate_ThenCreateSuccessful()
+        {
+            //?Given
+            var survey = GetSurvey();
+
+            //?When
+            await _surveyManager.Create(survey);
+
+            //?Then
+            _mockRepository.Verify(t => t.Create(It.Is<Survey>(e => MessageeIsWellCreated(e, survey))), Times.Once);
+        }
+
+        [Fact]
+        public async Task GivenSurvey_WhenUpdate_ThenUpdateSuccessful()
+        {
+            //?Given
+            var survey = GetSurvey();
+
+            //?When
+            await _surveyManager.Update(survey);
+
+            //?Then
+            _mockRepository.Verify(t => t.Update(It.Is<Survey>(e => MessageeIsWellUpdated(e, survey))), Times.Once);
+        }
+
+        [Fact]
+        public async Task GivenId_WhenDelete_ThenDeleteSuccessful()
+        {
+            //?Given
+            var id = "1234";
+
+            _mockRepository.Setup(x => x.Delete(id))
+                .Verifiable();
+
+            //?When
+            await _surveyManager.Delete(id);
+
+            //?Then
+            _mockRepository.Verify();
+        }
+
+        private Survey GetSurvey()
+        {
+            return new Survey
+            {
+                Id = "1234",
+                Question = "This is a question",
+                Options = new List<SurveyOption>
+                    {
+                        new SurveyOption
+                        {
+                             Option = "This is an option"
+                        }
+                    }
+            };
+        }
+
+        private bool MessageeIsWellCreated(Survey created, Survey survey)
+        {
+            return created.Id == survey.Id &&
+                created.Question == survey.Question &&
+                created.Options.FirstOrDefault().Option == survey.Options.FirstOrDefault().Option;
+        }
+
+        private bool MessageeIsWellUpdated(Survey created, Survey survey)
+        {
+            return created.Id == survey.Id &&
+                created.Question == survey.Question &&
+                created.Options.FirstOrDefault().Option == survey.Options.FirstOrDefault().Option;
+        }
+    }
+}
